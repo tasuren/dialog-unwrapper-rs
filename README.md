62b7a18bcb48c2a467bc308277126df12f91f63e
Takagi Tasuku <tasuren@outlook.jp>
2023-09-09 18:43:34 +0900

    恐らく完成

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
-**作業中**
-
 Rust製アプリのための、簡単にResult型をunwrapし、エラーであればダイアログでそれを伝えるためのクレートです。  
 そんな規模が大きくないのでcrates.ioには公開していませんが、要望があればそうします。
 
-## 要件
-現状、エラーはanyhowのもの
+## 使い方
+まず最初にAnyHowの`.context`メソッドでエラーダイアログの説明を設定します。  
+そのあと、トレイト`ErrorDialogUnwrapper`をインポートし、`unwrap_or_dialog`を使います。  
+使うトレイトはpreludeモジュールにあるもので全て揃います。（これは追加でanyhowのbailといったマクロもインポートします。）
+### 例
+```rust
+use dialog_unwrapper::prelude::*;
+
+fn calculate() -> Result<usize> {
+    unimplemented!();
+}
+
+fn main() {
+    println!(
+        "{}",
+        calculate()
+            .context("とあるプロセスが異常終了しました。")
+            .unwrap_or_dialog();
+    )
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,25 @@
-use std::fmt::{Display, Debug};
+use std::{
+    fmt::{Debug, Display},
+    sync::OnceLock,
+};
 
 use anyhow::Error;
 
-use rfd::{ MessageDialog, AsyncMessageDialog };
-use rust_i18n::t;
+use rfd::{AsyncMessageDialog, MessageDialog};
+pub use rfd;
 
+/// 予期せぬエラーを示す文字列を取得する関数。
+pub static GET_UNEXPECTED_ERROR_TEXT: OnceLock<Box<dyn Fn() -> String + Send + Sync>> =
+    OnceLock::new();
+/// 予期せぬエラーであることを示す文字列を格納するためのグローバル変数。
+/// 代わりに`GET_UNEXPECTED_ERROR_TEXT`で動的に変更可能。
+pub static UNEXPECTED_ERROR_TEXT: OnceLock<String> = OnceLock::new();
+
+/// `GET_UNEXPECTED_ERROR_TEXT`を使って予期せぬエラーのタイトルを取得します。
+/// もし設定されていない場合、デフォルトの"Unexpected Error"が取得されます。
+pub fn get_unexpected_error_text() -> String {
+    GET_UNEXPECTED_ERROR_TEXT.get_or_init(|| Box::new(|| String::from("Unexpected Error")))()
+}
 
 pub trait ErrorDialogUnwrapper<T, E = Error>: Sized {
     fn unwrap_or_dialog(self) -> T;
@@ -14,24 +29,15 @@ pub trait ErrorDialogUnwrapper<T, E = Error>: Sized {
     fn ok_unwrap_or_dialog_with_title(self, title: impl Display) -> Option<T>;
 }
 
-
-pub fn show_error_dialog(
-    title: String,
-    e: impl Debug,
-    async_: bool
-) -> (String, String) {
+pub fn show_error_dialog(title: String, e: impl Debug, async_: bool) -> (String, String) {
     let text = format!("{:?}", e);
 
     if async_ {
         let dialog = AsyncMessageDialog::new();
-        let _ = dialog.set_title(&title)
-            .set_description(&text)
-            .show();
+        let _ = dialog.set_title(&title).set_description(&text).show();
     } else {
         let dialog = MessageDialog::new();
-        dialog.set_title(&title)
-            .set_description(&text)
-            .show();
+        dialog.set_title(&title).set_description(&text).show();
     };
 
     (title, text)
@@ -45,18 +51,14 @@ impl<T, E: Debug> ErrorDialogUnwrapper<T, E> for Result<T, E> {
     fn unwrap_or_dialog(self) -> T {
         match self {
             Ok(v) => v,
-            Err(e) => quick_panic(show_error_dialog(
-                get_unexpected_error(), e, false
-            ))
+            Err(e) => quick_panic(show_error_dialog(get_unexpected_error_text(), e, false)),
         }
     }
 
     fn unwrap_or_dialog_with_title(self, title: impl Display) -> T {
         match self {
             Ok(v) => v,
-            Err(e) => quick_panic(show_error_dialog(
-                format!("{}", title), e, false
-            ))
+            Err(e) => quick_panic(show_error_dialog(format!("{}", title), e, false)),
         }
     }
 
@@ -64,7 +66,7 @@ impl<T, E: Debug> ErrorDialogUnwrapper<T, E> for Result<T, E> {
         match self {
             Ok(v) => Some(v),
             Err(e) => {
-                show_error_dialog(get_unexpected_error(), e, true);
+                show_error_dialog(get_unexpected_error_text(), e, true);
                 None
             }
         }
@@ -80,12 +82,6 @@ impl<T, E: Debug> ErrorDialogUnwrapper<T, E> for Result<T, E> {
         }
     }
 }
-
-
-pub fn get_unexpected_error() -> String {
-    t!("common.error.unexpected")
-}
-
 
 /// 指定されたタイトルと説明でエラー時にダイアログを表示する`unwrap`をラップした関数を生成します。
 #[macro_export]
@@ -110,9 +106,8 @@ macro_rules! define_unwrapper {
     };
 }
 
-
 pub mod prelude {
-    pub use anyhow::{Result, Error, Context as _, Ok, anyhow, bail};
     pub use super::ErrorDialogUnwrapper as _;
     pub use crate::define_unwrapper;
+    pub use anyhow::{anyhow, bail, Context as _, Error, Result};
 }
